c7c96dc8a3d450884f519f1f2d79e23131c66a96
Andreas Jensen <Andr4376@gmail.com>
2022-04-14 22:46:11 +0200

    health service

---
+++ Core/HealthService.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Core
+{
+    public interface IHealthService
+    {
+        /// <summary>
+        /// Read current health
+        /// </summary>
+        int CurrentHealth { get; }
+
+        /// <summary>
+        /// Read maximum possibly health
+        /// </summary>
+        int MaxHealth { get; }
+
+        /// <summary>
+        /// Health status
+        /// </summary>
+        bool IsAlive { get; }
+
+        /// <summary>
+        /// Interface for receiving damage
+        /// </summary>
+        /// <param name="dmg"></param>
+        void TakeDamage(int dmg);
+
+        /// <summary>
+        /// Interface for healing damage
+        /// </summary>
+        /// <param name="health"></param>
+        void Heal(int health);
+    }
+
+    [DebuggerDisplay("CurrentHealth:{CurrentHealth}. MaxHealth:{MaxHealth}")]
+    public partial class HealthService : IHealthService
+    {
+        private int health;
+
+        private Action? OnDeath = null;
+
+        /// <inheritdoc/>
+        public int CurrentHealth
+        {
+            get { return health; }
+            private set
+            {
+                health = value;
+                if (health < 0)
+                    health = 0;
+
+                if (health > MaxHealth)
+                    health = MaxHealth;
+
+                if (health == 0 && OnDeath != null)
+                {
+                    OnDeath.Invoke();
+                    OnDeath = null;
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        public bool IsAlive
+        {
+            get
+            {
+                return health > 0;
+            }
+        }
+
+        private int maxHealth;
+
+        public HealthService(int health, int maxHealth)
+        {
+            this.health = health;
+            this.maxHealth = maxHealth;
+        }
+        public HealthService(int health, int maxHealth, Action onDeath) : this(health, maxHealth)
+        {
+            this.OnDeath = onDeath;
+        }
+        public HealthService(int maxHealth) : this(maxHealth, maxHealth) { }
+        public HealthService(int maxHealth, Action onDeath) : this(maxHealth, maxHealth, onDeath) { }
+
+        /// <inheritdoc/>
+        public int MaxHealth
+        {
+            get { return maxHealth; }
+            private set { maxHealth = value; }
+        }
+
+
+        /// <inheritdoc/>
+        public void Heal(int healing)
+        {
+            if (healing < 0)
+                throw new ArgumentOutOfRangeException(nameof(healing) + " cannot be negative!");
+
+            if (!IsAlive)
+                return;
+
+            CurrentHealth += healing;
+        }
+
+        /// <inheritdoc/>
+        public void TakeDamage(int dmg)
+        {
+            if (dmg < 0)
+                throw new ArgumentOutOfRangeException(nameof(dmg) + " cannot be negative!");
+            CurrentHealth -= dmg;
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
---
+++ CoreTests/HealthServiceTests.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Core.Tests
+{
+    [TestClass()]
+    public class HealthServiceTests
+    {
+        [TestMethod()]
+        public void HealthService_props()
+        {
+            //arrange
+            int health = 50;
+            int maxHealth = 100;
+
+            //act
+            IHealthService svc = new HealthService(health, maxHealth);
+
+            //assert
+            Assert.AreEqual(health, svc.CurrentHealth);
+            Assert.AreEqual(maxHealth, svc.MaxHealth);
+        }
+
+        [TestMethod()]
+        public void HealthService_Heal()
+        {
+            //arrange
+            int health = 50;
+            int toHeal = 25;
+            //act
+            IHealthService svc = new HealthService(health, maxHealth: 10000);
+            svc.Heal(toHeal);
+
+            //assert
+            Assert.AreEqual(health + toHeal, svc.CurrentHealth);
+        }
+        [TestMethod()]
+        public void HealthServiceTest_Heal_cannotOverheal()
+        {
+            //arrange
+            int health = 50;
+            int toHeal = 9999;
+            //act
+            IHealthService svc = new HealthService(health);
+            svc.Heal(toHeal);
+
+            //assert
+            Assert.AreEqual(svc.MaxHealth, svc.CurrentHealth);
+        }
+
+        [TestMethod()]
+        public void HealthServiceTest_Heal_Negative_Exception()
+        {
+            //arrange
+            int health = 50;
+            int toHeal = -1;
+
+            //act
+            IHealthService svc = new HealthService(health);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => svc.Heal(toHeal));
+
+        }
+
+        [TestMethod()]
+        public void HealthServiceTest_TakeDamage_Negative_Exception()
+        {
+            //arrange
+            int health = 50;
+            int dmg = -1;
+
+            //act
+            IHealthService svc = new HealthService(health);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => svc.TakeDamage(dmg));
+
+        }
+
+        [TestMethod()]
+        public void HealthServiceTest_TakeDamage()
+        {
+            //arrange
+            int health = 50;
+            int dmg = 1;
+            //act
+            IHealthService svc = new HealthService(health);
+            svc.TakeDamage(dmg);
+
+            //assert
+            Assert.AreEqual(svc.MaxHealth - dmg, svc.CurrentHealth);
+        }
+
+        [TestMethod()]
+        public void HealthServiceTest_TakeDamage_cannotOverkill()
+        {
+            //arrange
+            int health = 50;
+            int dmg = 1000;
+            //act
+            IHealthService svc = new HealthService(health);
+            svc.TakeDamage(dmg);
+
+            //assert
+            Assert.AreEqual(0, svc.CurrentHealth);
+        }
+
+        [TestMethod()]
+        public void HealthServiceTest_TakeDamage_Killed_IsAliveFalse()
+        {
+            //arrange
+            int health = 50;
+            int dmg = 1000;
+            //act
+            IHealthService svc = new HealthService(health);
+            svc.TakeDamage(dmg);
+
+            //assert
+            Assert.AreEqual(0, svc.CurrentHealth);
+            Assert.IsFalse(svc.IsAlive);
+        }
+
+        [TestMethod()]
+        public void HealthServiceTest_OnDeath()
+        {
+            //arrange
+            int health = 50;
+            int dmg = 1000;
+
+            //act
+            int deathCount = 0;
+            IHealthService svc = new HealthService(health, () => { deathCount++; });
+            svc.TakeDamage(dmg);
+
+            //assert
+            Assert.AreEqual(1, deathCount);
+        }
+
+        [TestMethod()]
+        public void HealthServiceTest_OnDeath_DoesNotInvokeOnDeathMoreThanOnce()
+        {
+            //arrange
+            int health = 50;
+            int dmg = 1000;
+
+            //act
+            int deathCount = 0;
+            IHealthService svc = new HealthService(health, () => { deathCount++; });
+            svc.TakeDamage(dmg);
+            svc.Heal(1);
+            svc.TakeDamage(999999);
+
+            //assert
+            Assert.AreEqual(1, deathCount);
+        }
+    }
+}
